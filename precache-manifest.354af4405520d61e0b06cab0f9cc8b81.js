6d2a081e189405efca9099b3a41727a234022b96
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-01-30 15:37:50 +0800

    Updates

---
+++ precache-manifest.354af4405520d61e0b06cab0f9cc8b81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "ebd0610c4e510b97232e",
+    "url": "/my-app/static/js/main.ebd0610c.chunk.js"
+  },
+  {
+    "revision": "9e93e3db1cef01c7328e",
+    "url": "/my-app/static/js/1.9e93e3db.chunk.js"
+  },
+  {
+    "revision": "ebd0610c4e510b97232e",
+    "url": "/my-app/static/css/main.e0dafb22.chunk.css"
+  },
+  {
+    "revision": "c832d114549e8d22440d81a3465fcd15",
+    "url": "/my-app/index.html"
+  }
+];
